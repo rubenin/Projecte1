db94dda97b19e6a00a92af69c319e03a72b20072
Ruben <rubengonzalezglez@me.com>
2022-11-14 21:42:22 +0100

    tabulados

--- src/main/java/Setmana9/Projecte2.java
+++ src/main/java/Setmana9/Projecte2.java
@@ -7,9 +7,10 @@ import java.util.Scanner;
  * @author ruben
  */
 public class Projecte2 {
+
     //Definim totes les constants
     public static final int MINIM_ID = 111;
-    public static final int MAXIM_ID =  999;
+    public static final int MAXIM_ID = 999;
     public static final int MINIM_EDAT = 14;
     public static final int MAXIM_EDAT = 120;
     public static final int MINIM_TIPUS = 0;
@@ -27,8 +28,7 @@ public class Projecte2 {
     public static final String JOV_NOM = "Carnet Jove";
     public static final String SOC_NOM = "Soci";
     public static final int MAX_INTENTS = 3;
-    
-    
+
     @SuppressWarnings("empty-statement")
     public static void main(String[] args) {
         //Definim variables
@@ -42,10 +42,10 @@ public class Projecte2 {
         int seguir = 0;
         String tipusVenda = "";
         Scanner scan = new Scanner(System.in);
-        boolean dadaCorrecta = true; 
+        boolean dadaCorrecta = true;
         boolean introduirMes;
-       
-        do{
+
+        do {
             introduirMes = false; //per defecte, no volem introduir mes
             //si no ens indiquen el contrari
             //Comencem demanant l'id
@@ -55,127 +55,122 @@ public class Projecte2 {
                 //LLegim dades
                 id = scan.nextInt();
                 //Avaluem si és válida
-                if ((id < MINIM_ID)||(id > MAXIM_ID)){
+                if ((id < MINIM_ID) || (id > MAXIM_ID)) {
                     dadaCorrecta = false;
                 }
             }
             i++;
-            }while ((!dadaCorrecta)&&(i<MAX_INTENTS));
-        
-            if (dadaCorrecta){
-                i = 0;//Inicialitzem comptador
-                
-                do{
+        } while ((!dadaCorrecta) && (i < MAX_INTENTS));
+
+        if (dadaCorrecta) {
+            i = 0;//Inicialitzem comptador
+
+            do {
                 //Pedim edat, llegim i mirem si és correcta
                 System.out.print("Entra la edat: ");
                 dadaCorrecta = scan.hasNextInt();
-                
+
                 if (dadaCorrecta) {
                     //Llegim dades
                     edat = scan.nextInt();
                     //Avaluem si és válida
-                    if ((edat < MINIM_EDAT)||(edat > MAXIM_EDAT)){
+                    if ((edat < MINIM_EDAT) || (edat > MAXIM_EDAT)) {
                         dadaCorrecta = false;
                     }
-                }i++;
-            }while ((!dadaCorrecta)&&(i<MAX_INTENTS));
-         
-                if (dadaCorrecta){
-                    i = 0; // Inicialitzem el comptador
-                    do{
-                
-                        //Entrem tipus de venda
-                        System.out.print("\tVenda lliure (0)\n" +
-                                "\tPensionista (1)\n" +
-                                "\tCarnet jove (2)\n" +
-                                "\tSoci (3)\n\n" +
-                                "Entra el tipus de venda:");
+                }
+                i++;
+            } while ((!dadaCorrecta) && (i < MAX_INTENTS));
+
+            if (dadaCorrecta) {
+                i = 0; // Inicialitzem el comptador
+                do {
+
+                    //Entrem tipus de venda
+                    System.out.print("\tVenda lliure (0)\n"
+                            + "\tPensionista (1)\n"
+                            + "\tCarnet jove (2)\n"
+                            + "\tSoci (3)\n\n"
+                            + "Entra el tipus de venda:");
+                    dadaCorrecta = scan.hasNextInt();
+                    if (dadaCorrecta) {
+                        tipus = scan.nextInt();
+                        if ((tipus < MINIM_TIPUS) || (tipus > MAXIM_TIPUS)) {
+                            dadaCorrecta = false;
+                        }
+                    }
+                    i++;
+                } while ((!dadaCorrecta) && (i < MAX_INTENTS));
+
+                if (dadaCorrecta) {//Si fins al moment tot es correcte, pedim l'import
+                    i = 0;
+                    do {
+                        System.out.print("Entra l'import: ");
                         dadaCorrecta = scan.hasNextInt();
-                        if (dadaCorrecta){
-                            tipus = scan.nextInt();
-                            if ((tipus < MINIM_TIPUS)||(tipus > MAXIM_TIPUS)){
+                        if (dadaCorrecta) {//si es un número real  
+                            importe = scan.nextInt();
+                            if ((importe < MINIM_IMPORT) || (importe > MAXIM_IMPORT)) {
                                 dadaCorrecta = false;
                             }
                         }
                         i++;
-                    }while ((!dadaCorrecta)&&(i<MAX_INTENTS));
-                    
-                    if (dadaCorrecta){//Si fins al moment tot es correcte, pedim l'import
+                    } while ((!dadaCorrecta) && (i < MAX_INTENTS));
+
+                    if (dadaCorrecta) {
                         i = 0;
-                        do{
-                            System.out.print("Entra l'import: ");
+                        //Llegim el telefon
+                        do {
+                            System.out.print("Entra el telefon: ");
                             dadaCorrecta = scan.hasNextInt();
-                            if (dadaCorrecta){//si es un número real  
-                                importe = scan.nextInt();
-                                if ((importe < MINIM_IMPORT)||(importe > MAXIM_IMPORT)){
+                            if (dadaCorrecta) {
+                                telefon = scan.nextInt();
+                                if ((telefon < MINIM_TELEFON) || (telefon > MAXIM_TELEFON)) {
                                     dadaCorrecta = false;
                                 }
                             }
                             i++;
-                        }while ((!dadaCorrecta)&&(i<MAX_INTENTS));
-                        
+                        } while ((!dadaCorrecta) && (i < MAX_INTENTS));
+
                         if (dadaCorrecta) {
-                            i = 0;
-                            //Llegim el telefon
-                            do{
-                                System.out.print("Entra el telefon: ");
-                                    dadaCorrecta = scan.hasNextInt();
-                                    if (dadaCorrecta){
-                                        telefon = scan.nextInt();
-                                        if ((telefon < MINIM_TELEFON)||(telefon > MAXIM_TELEFON)){
-                                            dadaCorrecta = false;
-                                        }
-                                    }
-                                    i++;
-                                }while ((!dadaCorrecta)&&(i<MAX_INTENTS));
-                            
-                                if (dadaCorrecta) {
-                                    //Obtin tipus de venda
-                                    switch (tipus){
-                                        case LLI:
-                                            tipusVenda = LLI_NOM;
-                                            break;
-                                        case PEN:
-                                            tipusVenda = PEN_NOM;
-                                            break;
-                                        case JOV:
-                                            tipusVenda = JOV_NOM;
-                                            break;
-                                        case SOC:
-                                            tipusVenda = SOC_NOM;
-                                            break;
-                                        }
-                                        //Mostrem el resultat per pantalla
-                                        System.out.println("ID  ||  Edat    ||  Tipus de venda  ||  Import  ||  Telefon");
-                                        System.out.println(id+" ||  "+edat+"      ||  "+tipusVenda+"     ||  "+importe+"      ||  "+telefon);
-                                        clientsEntrats++;//comptador de clients
-                                        
-                                }else{//En cas de que hagi errors, donará error de dades
-                                        System.out.println("Error de dades");
-                                    }
-                                    //Avaluem si vol introduir més clients
-                                    System.out.println("es vol introduir més clients? (0 = no / 1 = si)");
-                                    dadaCorrecta = scan.hasNextInt();
-                                    if (dadaCorrecta){
-                                        seguir = scan.nextInt();
-                                        if (seguir == 1){
-                                            introduirMes = true;
-                                        }
-                                    }else{
-                                        System.out.println("Dada incorrecta");
-                                    }
-                                    }while(introduirMes);
-                                    //Mostrem dades introduides
-                                    System.out.println("S'han inscrit "+clientsEntrats+" nous clients");
+                            //Obtin tipus de venda
+                            switch (tipus) {
+                                case LLI:
+                                    tipusVenda = LLI_NOM;
+                                    break;
+                                case PEN:
+                                    tipusVenda = PEN_NOM;
+                                    break;
+                                case JOV:
+                                    tipusVenda = JOV_NOM;
+                                    break;
+                                case SOC:
+                                    tipusVenda = SOC_NOM;
+                                    break;
                             }
+                            //Mostrem el resultat per pantalla
+                            System.out.println("ID  ||  Edat    ||  Tipus de venda  ||  Import  ||  Telefon");
+                            System.out.println(id + " ||  " + edat + "      ||  " + tipusVenda + "     ||  " + importe + "      ||  " + telefon);
+                            clientsEntrats++;//comptador de clients
+
+                        } else {//En cas de que hagi errors, donará error de dades
+                            System.out.println("Error de dades");
                         }
+                        //Avaluem si vol introduir més clients
+                        System.out.println("es vol introduir més clients? (0 = no / 1 = si)");
+                        dadaCorrecta = scan.hasNextInt();
+                        if (dadaCorrecta) {
+                            seguir = scan.nextInt();
+                            if (seguir == 1) {
+                                introduirMes = true;
+                            }
+                        } else {
+                            System.out.println("Dada incorrecta");
+                        }
+                    }
+                    while (introduirMes);
+                    //Mostrem dades introduides
+                    System.out.println("S'han inscrit " + clientsEntrats + " nous clients");
                 }
             }
         }
-     
-
-
-
- 
-        
-  
+    }
+}
--- src/main/java/Setmana9/Projecte2ExempleAmp2.java
+++ src/main/java/Setmana9/Projecte2ExempleAmp2.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Setmana9;
+
 import java.util.Scanner;
 
 /**
@@ -10,6 +11,7 @@ import java.util.Scanner;
  * @author ruben
  */
 public class Projecte2ExempleAmp2 {
+
     public static final int MINIM_DNI = 1;
     public static final int MAXIM_DNI = 99999999;
     public static final int MINIM_FAMILIA = 1;
@@ -34,140 +36,145 @@ public class Projecte2ExempleAmp2 {
     public static final String ADI_NOM = "Assitència a la direcció";
     public static final int MINIM_NOTA = 0;
     public static final int MAXIM_NOTA = 10;
- 
+
     public static void main(String[] args) {
         //Definim tipus de dades
-        int dni=0, familia=0, cicle=0, alumnesEntrats=0, seguir=0;
-        float nota=0f;
-        String nomCicle="";
-        Scanner scan = new Scanner(System.in); 
-        boolean dadaCorrecta=true, introduirMes;
-        
-        do{
-            introduirMes=false; //per defecte, no volem introduir més 
+        int dni = 0, familia = 0, cicle = 0, alumnesEntrats = 0, seguir = 0;
+        float nota = 0f;
+        String nomCicle = "";
+        Scanner scan = new Scanner(System.in);
+        boolean dadaCorrecta = true, introduirMes;
+
+        do {
+            introduirMes = false; //per defecte, no volem introduir més 
             //(si no ens indiquen el contrari)
             //Llegim i mirem si és correcta
             System.out.print("Entra el DNI: ");
             dadaCorrecta = scan.hasNextInt();//per analitzar tipus de dada 
-            if ( dadaCorrecta ){ // si és un número sencer 
-            //Llegim dades
-            dni = scan.nextInt();
-            //Avaluem si és vàlida
-            if((dni < MINIM_DNI)||(dni > MAXIM_DNI)){
-            dadaCorrecta = false;
-            }
-        }
-        if(dadaCorrecta){
-            //Entrem Cicle
-            System.out.print("\n\tinformàtica i comunicacions (1)\n" +    
-           "\tadministració i gestió (2)\n" +
-            "\telectricitat i electrònica (3)\n" +
-            "\tsanitat (4)\n" +
-            "\tserveis socioculturals i a la comunitat (4)\n"+
-            "Entra la familia professional:");
-            dadaCorrecta = scan.hasNextInt();
-            if ( dadaCorrecta ){ 
-                familia = scan.nextInt();
-                if((familia < MINIM_FAMILIA)||(familia > MAXIM_FAMILIA)){
-                dadaCorrecta = false;
-                }else{//Si aribem aquí es que la dada familia és correcta
-                switch (familia){
-                    case FAMILIA_INFORMATICA:
-                    System.out.print("\n\tSist. m. i xarxes (111)\n"+
-                    "\tDesenv. d'aplicacions multiplataforma (121)\n"+
-                    "\tAdministració de sistemes informàtics (122)\n"+
-                    "\tDesenvolupament d'aplicacions web (123)\n"+
-                    "Entra el cicle:"); 
-                    break;
-                    case FAMILIA_ADMINISTRACIO:
-                    System.out.print("\n\tGestió administrativa (211)\n"+
-                    "\tAdministració i finances (221)\n"+
-                    "\tAssitència a la direcció (222)\n"+
-                    "Entra el cicle:"); 
-                    break;
-                    //Resta de families per definir
-                }
-                dadaCorrecta = scan.hasNextInt();
-                if ( dadaCorrecta ){ 
-                    cicle = scan.nextInt();
-                    switch (familia){
-                        case FAMILIA_INFORMATICA:
-                        switch (cicle){
-                            case SMX: case DAM: case ASX: case DAW: 
-                            break; //Si codi és correcte no fem res
-                            default: 
-                            dadaCorrecta = false;
-                        }
-                        break;
-                        case FAMILIA_ADMINISTRACIO:
-                        switch (cicle){
-                        case GAD: case AFI: case ADI: 
-                        break; //Si codi és correcte no fem res
-                        default: 
-                        dadaCorrecta = false;
-                        } 
-                        break;
-                        //Resta de families per definir
-                    } 
-                }
-                    }
-            }
-        }
-        if(dadaCorrecta){ //si fins al moment tot correcte, anem a llegir nota
-            System.out.print("Entra la nota mitjana: ");
-            dadaCorrecta = scan.hasNextFloat();//per analitzar tipus de dada 
-            if ( dadaCorrecta ){ // si és un número real 
-                nota = scan.nextFloat();
-                if((nota < MINIM_NOTA)||(nota > MAXIM_NOTA)){
+            if (dadaCorrecta) { // si és un número sencer 
+                //Llegim dades
+                dni = scan.nextInt();
+                //Avaluem si és vàlida
+                if ((dni < MINIM_DNI) || (dni > MAXIM_DNI)) {
                     dadaCorrecta = false;
                 }
             }
-        }
-        if(dadaCorrecta){
-            //Obtinc nom del cicle
-            switch (cicle){
-                case SMX:
-                nomCicle = SMX_NOM; 
-                break;
-                case DAM:
-                nomCicle = DAM_NOM; 
-                break;
-                case ASX:
-                nomCicle = ASX_NOM; 
-                break;
-                case DAW:
-                nomCicle = DAW_NOM; 
-                break;
-                case GAD:
-                nomCicle = GAD_NOM; 
-                break;
-                case AFI:
-                nomCicle = AFI_NOM; 
-                break;
-                case ADI:
-                nomCicle = ADI_NOM; 
-                break; 
-                //Resta de families per definir
+            if (dadaCorrecta) {
+                //Entrem Cicle
+                System.out.print("\n\tinformàtica i comunicacions (1)\n"
+                        + "\tadministració i gestió (2)\n"
+                        + "\telectricitat i electrònica (3)\n"
+                        + "\tsanitat (4)\n"
+                        + "\tserveis socioculturals i a la comunitat (4)\n"
+                        + "Entra la familia professional:");
+                dadaCorrecta = scan.hasNextInt();
+                if (dadaCorrecta) {
+                    familia = scan.nextInt();
+                    if ((familia < MINIM_FAMILIA) || (familia > MAXIM_FAMILIA)) {
+                        dadaCorrecta = false;
+                    } else {//Si aribem aquí es que la dada familia és correcta
+                        switch (familia) {
+                            case FAMILIA_INFORMATICA:
+                                System.out.print("\n\tSist. m. i xarxes (111)\n"
+                                        + "\tDesenv. d'aplicacions multiplataforma (121)\n"
+                                        + "\tAdministració de sistemes informàtics (122)\n"
+                                        + "\tDesenvolupament d'aplicacions web (123)\n"
+                                        + "Entra el cicle:");
+                                break;
+                            case FAMILIA_ADMINISTRACIO:
+                                System.out.print("\n\tGestió administrativa (211)\n"
+                                        + "\tAdministració i finances (221)\n"
+                                        + "\tAssitència a la direcció (222)\n"
+                                        + "Entra el cicle:");
+                                break;
+                            //Resta de families per definir
+                        }
+                        dadaCorrecta = scan.hasNextInt();
+                        if (dadaCorrecta) {
+                            cicle = scan.nextInt();
+                            switch (familia) {
+                                case FAMILIA_INFORMATICA:
+                                    switch (cicle) {
+                                        case SMX:
+                                        case DAM:
+                                        case ASX:
+                                        case DAW:
+                                            break; //Si codi és correcte no fem res
+                                        default:
+                                            dadaCorrecta = false;
+                                    }
+                                    break;
+                                case FAMILIA_ADMINISTRACIO:
+                                    switch (cicle) {
+                                        case GAD:
+                                        case AFI:
+                                        case ADI:
+                                            break; //Si codi és correcte no fem res
+                                        default:
+                                            dadaCorrecta = false;
+                                    }
+                                    break;
+                                //Resta de families per definir
+                            }
+                        }
+                    }
+                }
             }
-            System.out.println("DNI nota Cicle");
-            System.out.println(dni+" "+nota+" "+nomCicle);
-            alumnesEntrats++; //comptador número alumnes
-            }else{
+            if (dadaCorrecta) { //si fins al moment tot correcte, anem a llegir nota
+                System.out.print("Entra la nota mitjana: ");
+                dadaCorrecta = scan.hasNextFloat();//per analitzar tipus de dada 
+                if (dadaCorrecta) { // si és un número real 
+                    nota = scan.nextFloat();
+                    if ((nota < MINIM_NOTA) || (nota > MAXIM_NOTA)) {
+                        dadaCorrecta = false;
+                    }
+                }
+            }
+            if (dadaCorrecta) {
+                //Obtinc nom del cicle
+                switch (cicle) {
+                    case SMX:
+                        nomCicle = SMX_NOM;
+                        break;
+                    case DAM:
+                        nomCicle = DAM_NOM;
+                        break;
+                    case ASX:
+                        nomCicle = ASX_NOM;
+                        break;
+                    case DAW:
+                        nomCicle = DAW_NOM;
+                        break;
+                    case GAD:
+                        nomCicle = GAD_NOM;
+                        break;
+                    case AFI:
+                        nomCicle = AFI_NOM;
+                        break;
+                    case ADI:
+                        nomCicle = ADI_NOM;
+                        break;
+                    //Resta de families per definir
+                }
+                System.out.println("DNI nota Cicle");
+                System.out.println(dni + " " + nota + " " + nomCicle);
+                alumnesEntrats++; //comptador número alumnes
+            } else {
                 System.out.println("dades incorrectes");
-            }       
+            }
             //Avaluem si vol introduir més alumnes
             System.out.println("es vol introduir mes alumnes?");
             dadaCorrecta = scan.hasNextInt();
-            if ( dadaCorrecta ){ // si és un número sencer 
+            if (dadaCorrecta) { // si és un número sencer 
                 seguir = scan.nextInt();
-                if(seguir == 1){
-                introduirMes = true;
-                }   
-            }else{
-            System.out.println("dada incorrecta");
+                if (seguir == 1) {
+                    introduirMes = true;
+                }
+            } else {
+                System.out.println("dada incorrecta");
             }
-        }while(introduirMes);
+        } while (introduirMes);
         //Mostrem dades introduides
-        System.out.println("S'han inscrit "+alumnesEntrats+" nous alumnes");
-    } 
+        System.out.println("S'han inscrit " + alumnesEntrats + " nous alumnes");
+    }
 }
